e4d84d35539a70ff3ffe26d4615e58c6de5d2e70
Mr Af2 <54968909+panjsher@users.noreply.github.com>
2020-04-25 05:30:49 +0430

    Update and rename zeroboy.py to pjrhack.py

--- pjrhack.py
+++ pjrhack.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE - Hack  -OFFICAL------•◈•
+|__\~~) •||•Pjr - Hack   ---------------•◈•
+|__(-----\  •◈•------Mr  --- Af2  -----•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mMr Af2
+\033[1;96mTelgram \033[1;93m: \033[1;PjrHack
+\033[1;96mFacebook  \033[1;93m: \033[1; drafg
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/panjsher
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mMr Af2
+\033[1;96mTelegram \033[1;93m: \033[1;92mPjrHack
+\033[1;96mFacebook  \033[1;93m: \033[1;92m drafg
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/panjsher
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "af2"
+CorrectPassword = "af2"
 
 loop = 'true'
 while (loop == 'true'):
@@ -100,10 +100,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system('xdg-open https://www.youtube.com/channel/UCpkJt660_upnZRNjnuLFNEA')
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-open https://www.youtube.com/channel/UCpkJt660_upnZRNjnuLFNEA')
 
 def login():
 	os.system('clear')
